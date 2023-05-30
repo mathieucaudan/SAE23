8866fa43a9f99e431bd3351b1e4be08671986efe
Mat4 <132671331+mathieucaudan@users.noreply.github.com>
2023-05-30 09:47:46 +0200

    Add files via upload

---
+++ del_user.php
@@ -0,0 +1,44 @@
+<?php
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Récupérer les valeurs de l'ID et du mot de passe depuis le formulaire
+        $id = $_POST['id'];
+        $password = $_POST['password'];
+
+        // Charger les données JSON depuis le fichier users.json
+        $jsonData = file_get_contents('users.json');
+
+        // Convertir la chaîne JSON en tableau associatif
+        $data = json_decode($jsonData, true);
+
+        // Vérifier si l'utilisateur existe dans le tableau
+        $users = $data['users'];
+        $userIndex = null;
+        foreach ($users as $index => $user) {
+            if ($user['id'] === $id && $user['password'] === $password) {
+                $userIndex = $index;
+                break;
+            }
+        }
+
+        // Si l'utilisateur est trouvé, le supprimer du tableau
+        if ($userIndex !== null) {
+            unset($users[$userIndex]);
+
+            // Réindexer le tableau
+            $data['users'] = array_values($users);
+
+            // Convertir le tableau modifié en chaîne JSON
+            $jsonData = json_encode($data, JSON_PRETTY_PRINT);
+
+            // Enregistrer la chaîne JSON dans le fichier users.json
+            file_put_contents('users.json', $jsonData);
+
+            echo "Utilisateur supprimé avec succès.";
+        } else {
+            echo "Identifiant ou mot de passe incorrect.";
+        }
+    }
+    
+    
+?>
