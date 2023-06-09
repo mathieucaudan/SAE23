a02621658276a13a33f63c870c64bd3254d38180
Mat4 <132671331+mathieucaudan@users.noreply.github.com>
2023-06-09 09:14:47 +0200

    Update vitrine.php

--- vitrine.php
+++ vitrine.php
@@ -740,4 +740,56 @@ function partage() {
 
     echo "</div></div>";
     }
+function partenaire(){
+	echo '
+<br>
+<div class="container">
+<p> Ici, vous retrouverez les entreprises avec lequelles nous collaborons pour vous satisfaire. </p>
+</div>
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-3 ">
+                <div class="card border bg-dark text-white shadow-lg rounded-25">
+                    <img src="CroissyFruit.png" style="width:auto" class="card-img-top p-3" alt="CroissyFruit">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">CroissyFruit</h5>
+                        <p class="card-text"> CroissyFruit est une entreprise de vente de fruits et légumes, venant des 4 coins de la planète bleue. Nous avons décidé de faire un partenariat avec eux, pour leur exigeance et rigourosité, en terme de produits proposés chez eux; et aussi pour l\'impact média qui peuvent nous apporter. </p>
+                        <a href="https://www.croissy-fruits.fr/" target="_blank" class="btn btn-black rounded">Plus d\'infos</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card border bg-dark text-white shadow-lg rounded-25">
+                    <img src="Ateliernati.png" style="width:auto" class="card-img-top p-3" alt="Atelier.nati">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">Atelier.nati</h5>
+                        <p class="card-text">Nathalie étant une artisant, travaillant la terre, elle produit une grande partie de nos couvert avec son style bien a elle. Nous l\'avons rencontré grâce aux nombreuses expositions aux quelles elle a participé.</p>
+                        <a href="https://www.instagram.com/atelier.nati/" target="_blank" class="btn btn-black rouded">Plus d\'infos</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card border bg-dark text-white shadow-lg rounded-25">
+                    <img src="Lafermeduprebois.png" style="width:auto" class="card-img-top p-3" alt="La ferme du pre bois">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">La ferme du pre bois</h5>
+                        <p class="card-text"> Nous voulions pour notre viande, faire participer les producteurs locaux. Nous nous sommes alors tournés vers ce petit producteur et sommes très satisfait.</p>
+                        <a href="https://lafermeduprebois.bzh/" target="_blank" class="btn btn-black rounded">Plus d\'infos</a>
+                    </div>
+                </div>
+            </div> 
+             <div class="col-3">
+                <div class="card border bg-dark text-white shadow-lg rounded-25">
+                    <img src="Lafarinedemordelles.png" style="width:auto" class="card-img-top p-3" alt="La farine de Mordelles">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">La farine de Mordelles </h5>
+                        <p class="card-text"> Nous voulions controler la farine que nous aurions pris, pour offrir à nos client la meilleure qualité possible, tout en faisant participer les producteur locaux. </p>
+                        <a href="la-farine-de-mordelles.fr/index.php" target="_blank" class="btn btn-black rounded">Plus d\'infos</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';
+};
+
 ?>
